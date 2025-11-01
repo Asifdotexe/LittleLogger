8ca128bdeb3863845b496e398c57b7bae36761da
Asif Sayyed <asifdotexe@gmail.com>
2025-11-01 23:02:54 +0530

    #2 add annotations

--- src/tinylogger/constants.py
+++ src/tinylogger/constants.py
@@ -8,7 +8,7 @@ DEFAULT_LOG_FILE: str = "experiment_log.jsonl"
 # The string format for timestamps in the log.
 # This is the standard ISO 8601 format in UTC (Zulu time).
 # Example: "2025-10-31T14:30:05Z"
-TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+TIMESTAMP_FORMAT: str = "%Y-%m-%dT%H:%M:%SZ"
 
 # The number of decimal places to round the runtime to.
-RUNTIME_PRECISION = 6
+RUNTIME_PRECISION: int = 6
