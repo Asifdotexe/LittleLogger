c97a9ab1305359df64c2adae0ef0b9c855d56dfc
Asif Sayyed <asifdotexe@gmail.com>
2025-11-01 16:37:50 +0530

    #2 add custom exceptions

---
+++ src/tinylogger/exceptions.py
@@ -0,0 +1,35 @@
+"""
+Custom Exception for TinyLogger package
+"""
+
+
+class LoggerError(Exception):
+    """
+    Base exception for all the TinyLogger errors
+    """
+
+    pass
+
+
+class LoggerNonSerializableError(TypeError, LoggerError):
+    """
+    Raised when the data passed to logger can't be serialized to JSON
+
+    This helps distinguish between a user's `TypeError` and one that
+    occurs specifically during the logging process.
+    """
+
+    pass
+
+
+class LoggerWriteError(IOError, LoggerError):
+    """
+    Raised when logger fails to write the log file
+
+    This could be due to several conditions:
+        Permission
+        Full disk
+        Other file system issues
+    """
+
+    pass
